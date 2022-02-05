923a290b4f9da60f28c197a5893d781523059e77
Fatih Ozdemir <fatih.zdemr@gmail.com>
2022-02-05 06:58:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace array_class_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.Write(sayi);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.Write(sayi);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            Array.Clear(sayiDizisi,2,2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.Write(sayi);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.Write(sayi);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.Write(sayi);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
